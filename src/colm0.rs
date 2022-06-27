93dce39c4625fa57e0ab9d5e7ac7d93222345b49
Schmid7k <schmid7k@protonmail.com>
2022-06-27 14:34:29 +0200

    Began generalization of COLM

--- src/colm0.rs
+++ src/colm0.rs
@@ -1,30 +1,59 @@
 use super::arch::*;
 use super::primitives::*;
 use super::{DecryptionError, EncryptionError};
+use aead::NewAead;
 use aes::{Aes128Dec, Aes128Enc};
-use cipher::inout::NotEqualError;
-use cipher::{consts::U16, generic_array::GenericArray, BlockDecrypt, BlockEncrypt, KeyInit};
+use cipher::BlockSizeUser;
+use cipher::Key;
+use cipher::KeySizeUser;
+use cipher::{
+    consts::U16, generic_array::GenericArray, BlockCipher, BlockDecrypt, BlockEncrypt, KeyInit,
+};
 
 use core::simd::u8x16;
 
 /// COLM authenticated encryption with associated data.
 ///
 /// Representation of COLM0, no intermediate tag generation. (encryption-only)
-pub struct Colm0Enc {
-    aes: Aes128Enc,
+pub struct Colm0Enc<B> {
+    bc: B,
 }
 
-impl Colm0Enc {
-    pub fn new(key: &GenericArray<u8, U16>) -> Self {
-        Self {
-            aes: Aes128Enc::new(key),
-        }
-    }
+impl<B> KeySizeUser for Colm0Enc<B>
+where
+    B: KeyInit,
+{
+    type KeySize = B::KeySize;
+}
 
+impl<B> NewAead for Colm0Enc<B>
+where
+    B: BlockSizeUser<BlockSize = U16> + BlockEncrypt + KeyInit,
+{
+    type KeySize = B::KeySize;
+
+    fn new(key: &Key<Self>) -> Self {
+        B::new(key).into()
+    }
+}
+
+impl<B> From<B> for Colm0Enc<B>
+where
+    B: BlockSizeUser<BlockSize = U16> + BlockEncrypt,
+{
+    fn from(cipher: B) -> Self {
+        Self { bc: cipher }
+    }
+}
+
+impl<B> Colm0Enc<B>
+where
+    B: BlockCipher + BlockSizeUser<BlockSize = U16> + BlockEncrypt,
+{
     #[inline]
-    fn aes_encrypt(&self, _in: __m128i) -> __m128i {
+    fn bc_encrypt(&self, _in: __m128i) -> __m128i {
         let mut tmp = u8x16::from(byte_swap(_in));
-        self.aes.encrypt_block(tmp.as_mut_array().into());
+        self.bc.encrypt_block(tmp.as_mut_array().into());
         byte_swap(tmp.into())
     }
 
@@ -43,14 +72,14 @@ impl Colm0Enc {
             v = _mm_set_epi64x(0, i64::from_be_bytes(*nonce));
             v = byte_swap(v);
             v = _mm_xor_si128(v, delta);
-            v = self.aes_encrypt(v);
+            v = self.bc_encrypt(v);
 
             while len >= 16 {
                 delta = gf128_mul2(&delta);
                 block = _mm_loadu_si128(ad[out.._in].as_ptr() as *const __m128i);
                 block = byte_swap(block);
                 block = _mm_xor_si128(block, delta);
-                block = self.aes_encrypt(block);
+                block = self.bc_encrypt(block);
                 v = _mm_xor_si128(v, block);
 
                 len -= 16;
@@ -65,7 +94,7 @@ impl Colm0Enc {
                 block = _mm_loadu_si128(buf[0..16].as_ptr() as *const __m128i);
                 block = byte_swap(block);
                 block = _mm_xor_si128(block, delta);
-                block = self.aes_encrypt(block);
+                block = self.bc_encrypt(block);
                 v = _mm_xor_si128(v, block);
             }
 
@@ -93,7 +122,7 @@ impl Colm0Enc {
 
             let mut c = vec![0u8; remaining + 16];
 
-            ll = self.aes_encrypt(ll);
+            ll = self.bc_encrypt(ll);
             lup = ll;
             ldown = gf128_mul3(&gf128_mul3(&ll));
 
@@ -107,11 +136,11 @@ impl Colm0Enc {
                 inb = byte_swap(inb);
                 checksum = _mm_xor_si128(checksum, inb);
                 block = _mm_xor_si128(inb, lup);
-                block = self.aes_encrypt(block);
+                block = self.bc_encrypt(block);
 
                 rho(&mut block, &mut w);
 
-                block = self.aes_encrypt(block);
+                block = self.bc_encrypt(block);
                 block = _mm_xor_si128(block, ldown);
                 _mm_storeu_si128(c[out.._in].as_ptr() as *mut __m128i, byte_swap(block));
 
@@ -134,11 +163,11 @@ impl Colm0Enc {
             checksum = _mm_xor_si128(checksum, inb);
 
             block = _mm_xor_si128(checksum, lup);
-            block = self.aes_encrypt(block);
+            block = self.bc_encrypt(block);
 
             rho(&mut block, &mut w);
 
-            block = self.aes_encrypt(block);
+            block = self.bc_encrypt(block);
             block = _mm_xor_si128(block, ldown);
             _mm_storeu_si128(c[out.._in].as_ptr() as *mut __m128i, byte_swap(block));
             out += 16;
@@ -151,11 +180,11 @@ impl Colm0Enc {
             ldown = gf128_mul2(&ldown);
 
             block = _mm_xor_si128(checksum, lup);
-            block = self.aes_encrypt(block);
+            block = self.bc_encrypt(block);
 
             rho(&mut block, &mut w);
 
-            block = self.aes_encrypt(block);
+            block = self.bc_encrypt(block);
             block = _mm_xor_si128(block, ldown);
 
             _mm_storeu_si128(buf.as_ptr() as *mut __m128i, byte_swap(block));
@@ -191,7 +220,7 @@ impl Colm0Enc {
             let mut _in = 16;
             let mut remaining = m.len();
 
-            ll = self.aes_encrypt(ll);
+            ll = self.bc_encrypt(ll);
             lup = ll;
             ldown = gf128_mul3(&gf128_mul3(&ll));
 
@@ -205,11 +234,11 @@ impl Colm0Enc {
                 inb = byte_swap(inb);
                 checksum = _mm_xor_si128(checksum, inb);
                 block = _mm_xor_si128(inb, lup);
-                block = self.aes_encrypt(block);
+                block = self.bc_encrypt(block);
 
                 rho(&mut block, &mut w);
 
-                block = self.aes_encrypt(block);
+                block = self.bc_encrypt(block);
                 block = _mm_xor_si128(block, ldown);
                 _mm_storeu_si128(c[out.._in].as_ptr() as *mut __m128i, byte_swap(block));
 
@@ -232,11 +261,11 @@ impl Colm0Enc {
             checksum = _mm_xor_si128(checksum, inb);
 
             block = _mm_xor_si128(checksum, lup);
-            block = self.aes_encrypt(block);
+            block = self.bc_encrypt(block);
 
             rho(&mut block, &mut w);
 
-            block = self.aes_encrypt(block);
+            block = self.bc_encrypt(block);
             block = _mm_xor_si128(block, ldown);
             _mm_storeu_si128(c[out.._in].as_ptr() as *mut __m128i, byte_swap(block));
             out += 16;
@@ -249,11 +278,11 @@ impl Colm0Enc {
             ldown = gf128_mul2(&ldown);
 
             block = _mm_xor_si128(checksum, lup);
-            block = self.aes_encrypt(block);
+            block = self.bc_encrypt(block);
 
             rho(&mut block, &mut w);
 
-            block = self.aes_encrypt(block);
+            block = self.bc_encrypt(block);
             block = _mm_xor_si128(block, ldown);
 
             _mm_storeu_si128(buf.as_ptr() as *mut __m128i, byte_swap(block));
@@ -263,19 +292,84 @@ impl Colm0Enc {
     }
 }
 
+struct BcEnc<E> {
+    enc: E,
+}
+
+impl<E> KeySizeUser for BcEnc<E>
+where
+    E: KeyInit,
+{
+    type KeySize = E::KeySize;
+}
+
+impl<E> NewAead for BcEnc<E>
+where
+    E: BlockSizeUser<BlockSize = U16> + BlockEncrypt + KeyInit,
+{
+    type KeySize = E::KeySize;
+
+    fn new(key: &Key<Self>) -> Self {
+        E::new(key).into()
+    }
+}
+
+impl<E> From<E> for BcEnc<E>
+where
+    E: BlockSizeUser<BlockSize = U16> + BlockEncrypt,
+{
+    fn from(cipher: E) -> Self {
+        Self { enc: cipher }
+    }
+}
+
+struct BcDec<D> {
+    dec: D,
+}
+
+impl<D> KeySizeUser for BcDec<D>
+where
+    D: KeyInit,
+{
+    type KeySize = D::KeySize;
+}
+
+impl<D> NewAead for BcDec<D>
+where
+    D: BlockSizeUser<BlockSize = U16> + BlockDecrypt + KeyInit,
+{
+    type KeySize = D::KeySize;
+
+    fn new(key: &Key<Self>) -> Self {
+        D::new(key).into()
+    }
+}
+
+impl<D> From<D> for BcDec<D>
+where
+    D: BlockSizeUser<BlockSize = U16> + BlockDecrypt,
+{
+    fn from(cipher: D) -> Self {
+        Self { dec: cipher }
+    }
+}
 /// COLM authenticated encryption with associated data.
 ///
 /// Representation of COLM0, no intermediate tag generation. (decryption-only)
-pub struct Colm0Dec {
-    aes_dec: Aes128Dec,
-    aes_enc: Aes128Enc,
+pub struct Colm0Dec<E, D> {
+    enc: BcEnc<E>,
+    dec: BcDec<D>,
 }
 
-impl Colm0Dec {
+impl<E, D> Colm0Dec<E, D>
+where
+    E: BlockCipher + BlockSizeUser<BlockSize = U16> + BlockEncrypt + KeyInit,
+    D: BlockCipher + BlockSizeUser<BlockSize = U16> + BlockDecrypt + KeyInit,
+{
     pub fn new(key: &GenericArray<u8, U16>) -> Self {
         Self {
-            aes_dec: Aes128Dec::new(key),
-            aes_enc: Aes128Enc::new(key),
+            enc: BcEnc::new(key),
+            dec: BcDec::from(D),
         }
     }
 
@@ -441,7 +535,7 @@ impl Colm0Dec {
         c: &[u8],
         ad: &[u8],
         nonce: &[u8; 8],
-    ) -> Result<usize, NotEqualError> {
+    ) -> Result<usize, DecryptionError> {
         unsafe {
             let buf = [0u8; 16];
             let (mut w, mut block, mut lup, mut ldown, mut inb): (
@@ -459,7 +553,7 @@ impl Colm0Dec {
             let mut remaining = c.len() - 16;
 
             if c.len() < 16 {
-                return Err(NotEqualError);
+                return Err(DecryptionError::MissingTag);
             }
 
             ll = self.aes_encrypt(ll);
